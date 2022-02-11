04637f2785226d0cd13388f6033e735a8128cfde
K_atc <shiftx1026@gmail.com>
2022-02-12 02:00:27 +0900

    Make elf-rs compatible with nostd

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,36 @@
-use std::fs;
-use std::io;
-use std::path::Path;
-use std::io::Read;
-
-extern crate byteorder;
-
-pub mod types;
+#![no_std]
 
 #[macro_use]
 pub mod utils;
+pub mod types;
 
-pub struct File {
+use core::fmt;
+
+extern crate alloc;
+use alloc::vec::Vec;
+use alloc::vec;
+
+extern crate acid_io as io;
+#[allow(unused_imports)]
+use io::prelude::*;
+
+extern crate cstr_core;
+use cstr_core::CString;
+
+pub struct ElfFile {
     pub ehdr: types::FileHeader,
     pub phdrs: Vec<types::ProgramHeader>,
     pub sections: Vec<Section>,
 }
 
-impl std::fmt::Debug for File {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+impl fmt::Debug for ElfFile {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:?} {:?} {:?}", self.ehdr, self.phdrs, self.sections)
     }
 }
 
-impl std::fmt::Display for File {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+impl fmt::Display for ElfFile {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         try!(write!(f, "{{ {} }}", self.ehdr));
         try!(write!(f, "{{ "));
         for phdr in self.phdrs.iter() {
@@ -37,51 +44,49 @@ impl std::fmt::Display for File {
     }
 }
 
-#[derive(Debug)]
-pub enum ParseError {
-    IoError(io::Error),
-    InvalidMagic,
-    InvalidFormat(Option<std::string::FromUtf8Error>),
-    NotImplemented,
-}
-
-impl std::convert::From<std::io::Error> for ParseError {
-    fn from(e: std::io::Error) -> Self {
+impl core::convert::From<io::Error> for ParseError {
+    fn from(e: io::Error) -> Self {
         ParseError::IoError(e)
     }
 }
 
-impl std::convert::From<std::string::FromUtf8Error> for ParseError {
-    fn from(e: std::string::FromUtf8Error) -> Self {
-        ParseError::InvalidFormat(Some(e))
+impl core::convert::From<cstr_core::NulError> for ParseError {
+    fn from(e: cstr_core::NulError) -> Self {
+        ParseError::CStringError(e)
     }
 }
 
-impl File {
-    pub fn open_path<T: AsRef<Path>>(path: T) -> Result<File, ParseError> {
-        // Open the file for reading
-        let mut io_file = try!(fs::File::open(path));
+#[derive(Debug)]
+pub enum ParseError {
+    IoError(io::Error),
+    InvalidMagic,
+    InvalidIdent,
+    InvalidFormat,
+    NotImplemented,
+    CStringError(cstr_core::NulError),
+}
 
-        File::open_stream(&mut io_file)
-    }
-
-    pub fn open_stream<T: io::Read + io::Seek>(io_file: &mut T) -> Result<File, ParseError> {
+impl ElfFile {
+    pub fn parse<T: io::Read + io::Seek>(io_file: &mut T) -> Result<ElfFile, ParseError> {
         // Read the platform-independent ident bytes
         let mut ident = [0u8; types::EI_NIDENT];
         let nread = try!(io_file.read(ident.as_mut()));
 
         if nread != types::EI_NIDENT {
-            return Err(ParseError::InvalidFormat(None));
+            return Err(ParseError::InvalidIdent);
         }
 
         // Verify the magic number
-        if ident[0] != types::ELFMAG0 || ident[1] != types::ELFMAG1
-                || ident[2] != types::ELFMAG2 || ident[3] != types::ELFMAG3 {
+        if ident[0] != types::ELFMAG0
+            || ident[1] != types::ELFMAG1
+            || ident[2] != types::ELFMAG2
+            || ident[3] != types::ELFMAG3
+        {
             return Err(ParseError::InvalidMagic);
         }
 
         // Fill in file header values from ident bytes
-        let mut elf_f = File::new();
+        let mut elf_f = ElfFile::new();
         elf_f.ehdr.class = types::Class(ident[types::EI_CLASS]);
         elf_f.ehdr.data = types::Data(ident[types::EI_DATA]);
         elf_f.ehdr.osabi = types::OSABI(ident[types::EI_OSABI]);
@@ -104,11 +109,11 @@ impl File {
             shoff = try!(read_u64!(elf_f, io_file));
         }
 
-        let flags = try!(read_u32!(elf_f, io_file));
-        let ehsize = try!(read_u16!(elf_f, io_file));
-        let phentsize = try!(read_u16!(elf_f, io_file));
+        let _flags = try!(read_u32!(elf_f, io_file));
+        let _ehsize = try!(read_u16!(elf_f, io_file));
+        let _phentsize = try!(read_u16!(elf_f, io_file));
         let phnum = try!(read_u16!(elf_f, io_file));
-        let shentsize = try!(read_u16!(elf_f, io_file));
+        let _shentsize = try!(read_u16!(elf_f, io_file));
         let shnum = try!(read_u16!(elf_f, io_file));
         let shstrndx = try!(read_u16!(elf_f, io_file));
 
@@ -144,22 +149,21 @@ impl File {
             }
 
             elf_f.phdrs.push(types::ProgramHeader {
-                    progtype: progtype,
-                    offset:   offset,
-                    vaddr:    vaddr,
-                    paddr:    paddr,
-                    filesz:   filesz,
-                    memsz:    memsz,
-                    flags:    flags,
-                    align:    align,
-                });
+                progtype: progtype,
+                offset: offset,
+                vaddr: vaddr,
+                paddr: paddr,
+                filesz: filesz,
+                memsz: memsz,
+                flags: flags,
+                align: align,
+            });
         }
 
         // Parse the section headers
         let mut name_idxs: Vec<u32> = Vec::new();
         try!(io_file.seek(io::SeekFrom::Start(shoff)));
         for _ in 0..shnum {
-            let name: String = String::new();
             let shtype: types::SectionType;
             let flags: types::SectionFlag;
             let addr: u64;
@@ -193,26 +197,28 @@ impl File {
             }
 
             elf_f.sections.push(Section {
-                    shdr: types::SectionHeader {
-                        name:      name,
-                        shtype:    shtype,
-                        flags:     flags,
-                        addr:      addr,
-                        offset:    offset,
-                        size:      size,
-                        link:      link,
-                        info:      info,
-                        addralign: addralign,
-                        entsize:   entsize,
-                    },
-                    data: Vec::new(),
-                });
+                shdr: types::SectionHeader {
+                    name: CString::new("")?,
+                    shtype: shtype,
+                    flags: flags,
+                    addr: addr,
+                    offset: offset,
+                    size: size,
+                    link: link,
+                    info: info,
+                    addralign: addralign,
+                    entsize: entsize,
+                },
+                data: Vec::new(),
+            });
         }
 
         // Read the section data
         let mut s_i: usize = 0;
         loop {
-            if s_i == shnum as usize { break; }
+            if s_i == shnum as usize {
+                break;
+            }
 
             let off = elf_f.sections[s_i].shdr.offset;
             let size = elf_f.sections[s_i].shdr.size;
@@ -229,11 +235,14 @@ impl File {
         // Parse the section names from the string header string table
         s_i = 0;
         loop {
-            if s_i == shnum as usize { break; }
+            if s_i == shnum as usize {
+                break;
+            }
 
-            elf_f.sections[s_i].shdr.name = try!(utils::get_string(
+            elf_f.sections[s_i].shdr.name = utils::get_string(
                 &elf_f.sections[shstrndx as usize].data,
-                name_idxs[s_i] as usize));
+                name_idxs[s_i] as usize
+            );
 
             s_i += 1;
         }
@@ -253,7 +262,12 @@ impl File {
         Ok(symbols)
     }
 
-    fn parse_symbol(&self, io_section: &mut Read, symbols: &mut Vec<types::Symbol>, link: &[u8]) -> Result<(), ParseError> {
+    fn parse_symbol(
+        &self,
+        io_section: &mut dyn io::Read,
+        symbols: &mut Vec<types::Symbol>,
+        link: &[u8],
+    ) -> Result<(), ParseError> {
         let name: u32;
         let value: u64;
         let size: u64;
@@ -278,25 +292,25 @@ impl File {
         }
 
         symbols.push(types::Symbol {
-                name:    try!(utils::get_string(link, name as usize)),
-                value:   value,
-                size:    size,
-                shndx:   shndx,
-                symtype: types::SymbolType(info[0] & 0xf),
-                bind:    types::SymbolBind(info[0] >> 4),
-                vis:     types::SymbolVis(other[0] & 0x3),
-            });
+            name: utils::get_string(link, name as usize),
+            value: value,
+            size: size,
+            shndx: shndx,
+            symtype: types::SymbolType(info[0] & 0xf),
+            bind: types::SymbolBind(info[0] >> 4),
+            vis: types::SymbolVis(other[0] & 0x3),
+        });
         Ok(())
     }
 
-    pub fn get_section<T: AsRef<str>>(&self, name: T) -> Option<&Section> {
+    pub fn get_section(&self, name: &CString) -> Option<&Section> {
         self.sections
             .iter()
-            .find(|section| section.shdr.name == name.as_ref() )
+            .find(|section| &section.shdr.name == name)
     }
 
-    pub fn new() -> File {
-        File {
+    pub fn new() -> ElfFile {
+        ElfFile {
             ehdr: types::FileHeader::new(),
             phdrs: Vec::new(),
             sections: Vec::new(),
@@ -310,21 +324,21 @@ pub struct Section {
     pub data: Vec<u8>,
 }
 
-impl std::fmt::Display for Section {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+impl fmt::Display for Section {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.shdr)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::path::PathBuf;
-    use File;
-
     #[test]
+    #[cfg(feature = "std")]
     fn test_open_path() {
-        let file = File::open_path(PathBuf::from("tests/samples/test1"))
-            .expect("Open test1");
+        use std::path::PathBuf;
+        use File;
+
+        let file = File::open_path(PathBuf::from("tests/samples/test1")).expect("Open test1");
         let bss = file.get_section(".bss").expect("Get .bss section");
         assert!(bss.data.iter().all(|&b| b == 0));
     }
--- src/utils.rs
+++ src/utils.rs
@@ -1,54 +1,66 @@
+use cstr_core::{CString, CStr};
+
+#[macro_export]
+macro_rules! try {
+    ($expr:expr) => {{
+        $expr?
+    }}
+}
+
 #[macro_export]
 macro_rules! read_u16 {
-    ($elf:ident, $io:ident) => ({
-        use byteorder::{LittleEndian, BigEndian, ReadBytesExt};
+    ($elf:ident, $io:ident) => {{
+        #[allow(unused_imports)]
+        use io::byteorder::{BigEndian, LittleEndian, ReadBytesExt};
         match $elf.ehdr.data {
-            types::ELFDATA2LSB => { $io.read_u16::<LittleEndian>() }
-            types::ELFDATA2MSB => { $io.read_u16::<BigEndian>() }
-            types::ELFDATANONE => { panic!("Unable to resolve file endianness"); }
-            _ => { panic!("Unable to resolve file endianness"); }
+            types::ELFDATA2LSB => $io.read_u16::<LittleEndian>(),
+            types::ELFDATA2MSB => $io.read_u16::<BigEndian>(),
+            types::ELFDATANONE => {
+                panic!("Unable to resolve file endianness");
+            }
+            _ => {
+                panic!("Unable to resolve file endianness");
+            }
         }
-    });
+    }};
 }
 
 #[macro_export]
 macro_rules! read_u32 {
-    ($elf:ident, $io:ident) => ({
-        use byteorder::{LittleEndian, BigEndian, ReadBytesExt};
+    ($elf:ident, $io:ident) => {{
+        #[allow(unused_imports)]
+        use io::byteorder::{BigEndian, LittleEndian, ReadBytesExt};
         match $elf.ehdr.data {
-            types::ELFDATA2LSB => { $io.read_u32::<LittleEndian>() }
-            types::ELFDATA2MSB => { $io.read_u32::<BigEndian>() }
-            types::ELFDATANONE => { panic!("Unable to resolve file endianness"); }
-            _ => { panic!("Unable to resolve file endianness"); }
+            types::ELFDATA2LSB => $io.read_u32::<LittleEndian>(),
+            types::ELFDATA2MSB => $io.read_u32::<BigEndian>(),
+            types::ELFDATANONE => {
+                panic!("Unable to resolve file endianness");
+            }
+            _ => {
+                panic!("Unable to resolve file endianness");
+            }
         }
-    });
+    }};
 }
 
 #[macro_export]
 macro_rules! read_u64 {
-    ($elf:ident, $io:ident) => ({
-        use byteorder::{LittleEndian, BigEndian, ReadBytesExt};
+    ($elf:ident, $io:ident) => {{
+        #[allow(unused_imports)]
+        use io::byteorder::{BigEndian, LittleEndian, ReadBytesExt};
         match $elf.ehdr.data {
-            types::ELFDATA2LSB => { $io.read_u64::<LittleEndian>() }
-            types::ELFDATA2MSB => { $io.read_u64::<BigEndian>() }
-            types::ELFDATANONE => { panic!("Unable to resolve file endianness"); }
-            _ => { panic!("Unable to resolve file endianness"); }
+            types::ELFDATA2LSB => $io.read_u64::<LittleEndian>(),
+            types::ELFDATA2MSB => $io.read_u64::<BigEndian>(),
+            types::ELFDATANONE => {
+                panic!("Unable to resolve file endianness");
+            }
+            _ => {
+                panic!("Unable to resolve file endianness");
+            }
         }
-    });
+    }};
 }
 
-use std;
-pub fn get_string(data: &[u8], start: usize) -> Result<String, std::string::FromUtf8Error> {
-    let mut end: usize = 0;
-    for i in start..data.len() {
-        if data[i] == 0u8 {
-            end = i;
-            break;
-        }
-    }
-    let mut rtn = String::with_capacity(end - start);
-    for i in start..end {
-        rtn.push(data[i] as char);
-    }
-    Ok(rtn)
+pub fn get_string(data: &[u8], start: usize) -> CString {
+    CString::from(unsafe { CStr::from_bytes_with_nul_unchecked(&data[start..data.len()])})
 }
